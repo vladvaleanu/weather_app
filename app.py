59285bec530a2329404c1347c2add340d5d474fa
vladvaleanu <vlad.valeanu@icloud.com>
2023-06-23 03:44:27 +0300

    Restructure code

--- app.py
+++ app.py
@@ -5,21 +5,30 @@ import os
 def configure():
     load_dotenv()
 
-configure()
-
 def kelvin_to_celsius(tempk):
     return round(tempk - 273.15)
 
+def get_weather_data(city):
+    api_key = os.getenv('api_key')
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    response = requests.get(url)
+    return response.json()
+
+configure()
+
 user_input = input("Enter the city name: ")
 
-weather_data = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + user_input + "&appid=" + os.getenv('api_key')).json()
+weather_data = get_weather_data(user_input)
 
-if weather_data['cod'] == "404":
+if weather_data.get('cod') == "404":
     print("City not found")
     exit()
 else:
     weather = weather_data['weather'][0]['main']
     temperature = kelvin_to_celsius(weather_data['main']['temp'])
 
-    print("The weather in " + user_input.capitalize() + " is " + weather + " with a temperature of " + str(temperature) + " degrees Celsius.")
-        
+    city_name = user_input.capitalize()
+    temperature_str = str(temperature) + " degrees Celsius"
+    message = f"The weather in {city_name} is {weather} with a temperature of {temperature_str}."
+
+    print(message)
